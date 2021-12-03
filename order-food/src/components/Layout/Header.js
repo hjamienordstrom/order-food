688245e051cbd62fb202fde433535ba4d663f8ba
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-12-02 20:57:02 -0500

    render dummy food

---
+++ order-food/src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import {Fragment} from "react";
+import mealsImage from '../../assets/meals.jpeg'
+import classes from './Header.module.css'
+import HeaderCardButton from "./HeaderCartButton";
+
+const Header = (props) => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Yummy</h1>
+            <HeaderCardButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table full of delicious food!"/>
+        </div>
+    </Fragment>
+};
+
+export default Header
