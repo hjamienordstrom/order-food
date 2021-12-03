69c776dac5af2cb50db648be05a929e88603f2b6
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-12-03 10:56:32 -0500

    cart toggle

--- order-food/src/components/Cart/Cart.js
+++ order-food/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import Model from '../UI/Model';
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -10,19 +10,17 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Model>
+  return <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </Model>
-  );
+    </Modal>
 };
 
 export default Cart;
