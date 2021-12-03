7ec1ac9469e4d283aa632248b4f1bf5ab53b35da
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-12-03 11:22:01 -0500

    cart reducer

--- order-food/src/store/CartProvider.js
+++ order-food/src/store/CartProvider.js
@@ -1,11 +1,38 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+      return{
+          items:updatedItems,
+          totalAmount:updatedTotalAmount
+      }
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeIitemFromCartHandler = (id) => {};
+  const [cartState, dispatchCardAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCardAction({ type: "ADD", item: item });
+  };
+  const removeIitemFromCartHandler = (id) => {
+    dispatchCardAction({ type: "REMOVE", id: id });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeIitemFromCartHandler,
   };
